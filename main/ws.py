5d975f049e91f017606169e40c79abb91400cbac
Ross Scanlon <info@4x4falcon.com>
2020-09-17 22:04:50 +1000

    Start of class creation

--- main/ws.py
+++ main/ws.py
@@ -1,14 +1,12 @@
 #! /usr/bin/env python
 #
-# MicroPython OTA Updater Example Module
-#
-# This file is part of micropython-ota-updater-example-module. 
-# https://github.com/bensherlock/micropython-ota-updater-example-module
-#
+# Weather Station OTA class
 #
 # MIT License
 #
-# Copyright (c) 2020 Benjamin Sherlock <benjamin.sherlock@ncl.ac.uk>
+# Copyright (c) 2020 Ross Scanlon <info@4x4falcon.com>
+#
+# based on MicroPython OTA Updater Example Module
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +17,7 @@
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,22 +26,23 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
-#"""MicroPython OTA Updater Example Module."""
+"""Weather Station OTA Class"""
 
 from machine import Pin
 
-class ExampleModuleClass:
-    """An Example Module Class"""
+class WeatherStationClass:
+	"""Weather Station Class"""
 
-    def __init__(self):
-	self.led = Pin(2, Pin.OUT)
-        pass
+	def __init__(self):
+		self.led = Pin(2, Pin.OUT)
+		pass
 
-    def __call__(self):
-        return self
+	def __call__(self):
+		return self
 
-    def do_something(self):
-        """Do something."""
-        print("Hello from Example Module Class.")
-	self.led.value(not self.led.value())
+	def do_it(self):
+		"""This is the main loop of the class"""
+		print("Hello from Weather Station Class.")
+
+		self.led.value(not self.led.value())
 
